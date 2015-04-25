59c28acde89b8d6fb5c4295148af5ea5cb8cb4f4
eashtian <eashtian@EASHTIAN-MOBL1.amr.corp.intel.com>
2015-04-25 02:35:53 -0700

    remove /*global*/

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 /*jslint node:true, vars:true, bitwise:true, unparam:true */
 /*jshint unused:true */
-/*global */
 
 /*
 A simple node.js application intended to blink the onboard LED on the Intel based development boards such as the Intel(R) Galileo and Edison with Arduino breakout board.
