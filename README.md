66721ac8b953d77db22135fbece8de3f0796b17f
Paul Fischer <paul.a.fischer@intel.com>
2016-10-20 17:17:39 -0700

    Major update. Updated to support Intel Joule target. See detailed description.
    - renamed <project-name>.xdk file for consistency
    - updated <project-name>.xdk file contents to 3.0.0 spec
    - updated and/or added .gitignore that reflects project archive needs
    - updated app.json to reflect need for "runtime", "targets" and "keywords"
    - cleaned up app.json to remove unused properties
    - updated README.md for consistency with other samples
    - updated and/or added LICENSE.md for consistency and completeness

--- README.md
+++ README.md
@@ -1,53 +1,36 @@
-Onboard LED Blink App
-============================
-A simple node.js application intended to blink the onboard LED on the Intel based development boards such as the Intel(R) Galileo and Edison with Arduino breakout board.
+Intel® XDK IoT Onboard LED Blink Node.js* App
+=============================================
 
-Intel(R) XDK 
--------------------------------------------
-This template is part of the Intel(R) XDK IoT Edition. 
-Download the Intel(R) XDK at http://software.intel.com/en-us/html5. To see the technical details of the sample, 
-please visit the sample article page at TBA.
+See [LICENSE.md](LICENSE.md) for license terms and conditions.
 
+This sample application is distributed as part of the
+[Intel® XDK](http://xdk.intel.com). It can also be downloaded
+or cloned directly from its git repo on the
+[public Intel XDK GitHub\* site](https://github.com/gomobile).
 
-Important App Files
----------------------------
+For help getting started developing applications with the
+Intel XDK, please start with
+[the Intel XDK documentation](https://software.intel.com/en-us/xdk/docs).
+
+App Overview
+------------
+A simple Node.js application that blinks the onboard LED, on
+select Intel IoT development boards.
+
+Important Sample App Files
+--------------------------
 * main.js
 * package.json
-* icon.png
+
+Important Sample Project Files
+------------------------------
 * README.md
+* LICENSE.md
+* project-name.xdk
+* project-name.xdke
 
-License Information Follows
----------------------------
-Copyright (c) 2014, Intel Corporation. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
-- Redistributions of source code must retain the above copyright notice, 
-  this list of conditions and the following disclaimer.
-
-- Redistributions in binary form must reproduce the above copyright notice, 
-  this list of conditions and the following disclaimer in the documentation 
-  and/or other materials provided with the distribution.
-
-- Neither the name of Intel Corporation nor the names of its contributors 
-  may be used to endorse or promote products derived from this software 
-  without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
-LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
-OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-mraa
---------------------------------------------
-* Included on the IoTDevkit Linux Image 
-
-* source:  https://github.com/intel-iot-devkit/mraa
-* license:  https://github.com/intel-iot-devkit/mraa/blob/9d488c8e869e59e1dff2c68218a8f38e9b959cd7/cmake/modules/LICENSE_1_0.txt
+Tested IoT Node.js Platforms
+----------------------------
+* [Intel® Galileo Board](http://intel.com/galileo)
+* [Intel® Edison Development Platform](http://intel.com/edison)
+* [Intel® Joule™ 570x Developer Kit](http://intel.com/joule)
--- main.js
+++ main.js
@@ -1,34 +1,86 @@
-/*jslint node:true, vars:true, bitwise:true, unparam:true */
-/*jshint unused:true */
-
 /*
-A simple node.js application intended to blink the onboard LED on the Intel based development boards such as the Intel(R) Galileo and Edison with Arduino breakout board.
+ * A simple Node.js application to blink the onboard LED.
+ * Supported Intel IoT development boards are identified in the code.
+ *
+ * https://software.intel.com/en-us/html5/articles/intel-xdk-iot-edition-nodejs-templates
+ */
 
-MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
-Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
 
-Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
-Using a ssh client: 
-1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
-2. opkg update
-3. opkg upgrade
+// keep these lines (below) for proper jshinting and jslinting
+/*jslint node:true */
+/*jshint unused:true */
+// see http://www.jslint.com/help.html and http://jshint.com/docs
 
-Article: https://software.intel.com/en-us/html5/articles/intel-xdk-iot-edition-nodejs-templates
-*/
 
-var mraa = require('mraa'); //require mraa
-console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the Intel XDK console
+var APP_NAME = 'IoT LED Blink' ;                        // display name of this app
+var MIN_MRAA_VERSION = '1.3.0' ;                        // minimum required MRAA version
 
-//var myOnboardLed = new mraa.Gpio(3, false, true); //LED hooked up to digital pin (or built in pin on Galileo Gen1)
-var myOnboardLed = new mraa.Gpio(13); //LED hooked up to digital pin 13 (or built in pin on Intel Galileo Gen2 as well as Intel Edison)
-myOnboardLed.dir(mraa.DIR_OUT); //set the gpio direction to output
-var ledState = true; //Boolean to hold the state of Led
+var mraa = require('mraa');                             // we need the mraa low-level I/O library
+var ver = require('./version-compare.js') ;             // for comparing semVer values
 
-periodicActivity(); //call the periodicActivity function
+console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n') ;   // poor man's clear console function
+console.log('\nEntering ' + APP_NAME) ;                 // announce our intentions! :-)
 
-function periodicActivity()
-{
-  myOnboardLed.write(ledState?1:0); //if ledState is true then write a '1' (high) otherwise write a '0' (low)
-  ledState = !ledState; //invert the ledState
-  setTimeout(periodicActivity,1000); //call the indicated function after 1 second (1000 milliseconds)
+var intervalID ;                                        // object to track periodic interval function
+var myOnboardLed ;                                      // to hold mraa LED I/O object
+var ledState ;                                          // boolean to hold the state of the LED
+
+
+// confirm that we have a version of MRAA that works
+// with this version of the app, exit if we do not
+
+if( ver.versionCompare(mraa.getVersion(), MIN_MRAA_VERSION) === false) {
+    console.error('Bad MRAA version string: ' + mraa.getVersion()) ;
+    process.exit(mraa.getVersion()) ;
 }
+if( ver.versionCompare(mraa.getVersion(), MIN_MRAA_VERSION) < 0 ) {
+    console.error('MRAA version is too old, upgrade your MRAA node module.') ;
+    console.error('MRAA version: ' + mraa.getVersion()) ;
+    process.exit(mraa.getVersion()) ;
+}
+
+
+// using the mraa library, detect which platform we are running on
+// and make appropriate adjustments to our gpio configuration calls
+// if we do not recognize the platform, issue error and exit the app
+
+switch( mraa.getPlatformType() ) {
+
+    case mraa.INTEL_GALILEO_GEN1:                       // Gallileo Gen 1
+        myOnboardLed = new mraa.Gpio(3, false, true) ;  // GPIO pin to drive onboard LED
+        break ;
+
+    case mraa.INTEL_GALILEO_GEN2:                       // Gallileo Gen 2
+    case mraa.INTEL_EDISON_FAB_C:                       // Edison
+        myOnboardLed = new mraa.Gpio(13) ;              // GPIO pin to drive onboard LED
+        break ;
+
+    case mraa.INTEL_GT_TUCHUCK:                         // Joule (aka Grosse Tete)
+        myOnboardLed = new mraa.Gpio(100) ;             // GPIO pin to drive onboard LED
+        break ;                                         // GPIO 100, 101, 102 or 103 will work
+
+    default:
+        console.error("Unknown MRAA platform: " + mraa.getPlatformType() + " -> " + mraa.getPlatformName()) ;
+        process.exit(mraa.getPlatformType()) ;
+}
+myOnboardLed.dir(mraa.DIR_OUT) ;                        // set the GPIO direction to output
+
+
+// now we are going to flash the LED
+// by simply toggling it at a periodic interval
+
+var periodicActivity = function() {
+    var ledState = myOnboardLed.read() ;                // get the current state of the LED pin
+    myOnboardLed.write(ledState?0:1) ;                  // if the pin is currently 1 write a '0' (low) else write a '1' (high)
+    process.stdout.write(ledState?'0':'1') ;            // and write an unending stream of toggling 1/0's to the console
+} ;
+intervalID = setInterval(periodicActivity, 1000) ;      // start the periodic toggle
+
+
+// type process.exit(0) in debug console to see
+// the following message be emitted to the debug console
+
+process.on('exit', function(code) {
+    clearInterval(intervalID) ;
+    console.log('\nExiting ' + APP_NAME + ', with code:', code) ;
+}) ;
