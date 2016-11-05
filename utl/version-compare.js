02f440b5135d18493215fda3e80ebc257d302011
Paul Fischer <paul.a.fischer@intel.com>
2016-11-04 17:26:23 -0700

    Minor cleanup and relocate to utl sub-directory.

--- utl/version-compare.js
+++ utl/version-compare.js
@@ -1,7 +1,15 @@
 // modification of https://gist.github.com/alexey-bass/1115557
 // Modifications, Copyright (c) 2016, Paul Fischer, Intel Corporation
 // converted so it can be "required" in node.js app
-// ignore leading 'v' character in version string (mraa does this)
+// ignore leading 'v' character in version string (mraa adds this)
+// cleanup minor jshint/jslint errors
+// add use strict control string
+
+// keep /*jslint and /*jshint lines for proper jshinting and jslinting
+// see http://www.jslint.com/help.html and http://jshint.com/docs
+/* jslint node:true */
+/* jshint unused:true */
+
 
 /**
  * Simply compares two string version values.
@@ -27,12 +35,14 @@
  */
 
 exports.versionCompare = function(left, right) {
+"use strict" ;
+
     if (typeof left + typeof right != 'stringstring')
         return false;
 
-    var a = left.split('.')
-    ,   b = right.split('.')
-    ,   i = 0, len = Math.max(a.length, b.length);
+    var a = left.split('.') ;
+    var b = right.split('.') ;
+    var i = 0, len = Math.max(a.length, b.length) ;
 
     if( a[0].substring(0,1) === 'v' )
         a[0] = a[0].substring(1) ;
