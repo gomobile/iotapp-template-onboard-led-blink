f551bdce0812085fe980c615c700ea5ecefbe5e4
Paul Fischer <paul.a.fischer@intel.com>
2017-01-15 12:08:44 -0800

    Add BSD license in header and modify version compare to also handle “pre-release” strings.
    Validation of the comparison of version strings that include “pre-release” strings is not complete, so version strings that include such strings may not result in a correct comparison, per semver.org.

--- utl/version-compare.js
+++ utl/version-compare.js
@@ -1,61 +1,117 @@
-// modification of https://gist.github.com/alexey-bass/1115557
-// Modifications, Copyright (c) 2016, Paul Fischer, Intel Corporation
-// converted so it can be "required" in node.js app
-// ignore leading 'v' character in version string (mraa adds this)
-// cleanup minor jshint/jslint errors
-// add use strict control string
+/*
+ * Copyright (c) 2016-2017, Paul Fischer, Intel Corporation
+ *
+ * This is a “BSD-3” license. <http://www.tldrlegal.com/l/bsd3>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * - Neither the name of Intel Corporation nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-// keep /*jslint and /*jshint lines for proper jshinting and jslinting
+// spec jslint and jshint lines for desired JavaScript linting
 // see http://www.jslint.com/help.html and http://jshint.com/docs
 /* jslint node:true */
 /* jshint unused:true */
 
+"use strict" ;
+
 
 /**
- * Simply compares two string version values.
+ * Compares two simple semver version strings.
+ * This is a very simple comparator that does not implement
+ * the complete semver specification! It may have issues when
+ * an ASCII string is part of the comparison.
  *
- * Example:
+ * Examples:
+ *
  * versionCompare('1.1', '1.2') => -1
  * versionCompare('1.1', '1.1') =>  0
  * versionCompare('1.2', '1.1') =>  1
  * versionCompare('2.23.3', '2.22.3') => 1
  *
  * Returns:
+ *
  * -1 = left is LOWER than right
- *  0 = they are equal
- *  1 = left is GREATER = right is LOWER
- *  And FALSE if one of input versions are not valid
+ *  0 = both strings are equal
+ *  1 = left is GREATER than right
+ *  FALSE if at least one of the inputs is not a string.
  *
  * @function
- * @param {String} left  Version #1
- * @param {String} right Version #2
+ * @param {String} left - version string
+ * @param {String} right - version string
  * @return {Integer|Boolean}
- * @author Alexey Bass (albass)
- * @since 2011-07-14
  */
 
 exports.versionCompare = function(left, right) {
-"use strict" ;
+    var i, len ;
+    var arr1 = [], arr2 = [] ;
 
-    if (typeof left + typeof right != 'stringstring')
-        return false;
+    if( (typeof left + typeof right) !== "stringstring" )
+        return false ;
 
-    var a = left.split('.') ;
-    var b = right.split('.') ;
-    var i = 0, len = Math.max(a.length, b.length) ;
+    arr1 = left.split(/[\-\.]/) ;
+    arr2 = right.split(/[\-\.]/) ;
 
-    if( a[0].substring(0,1) === 'v' )
-        a[0] = a[0].substring(1) ;
-    if( b[0].substring(0,1) === 'v' )
-        b[0] = b[0].substring(1) ;
+    // ignore leading 'v' character in version string (libmraa adds this)
+    if( arr1[0].substring(0,1) === 'v' )
+        arr1[0] = arr1[0].substring(1) ;
+    if( arr2[0].substring(0,1) === 'v' )
+        arr2[0] = arr2[0].substring(1) ;
 
-    for (; i < len; i++) {
-        if ((a[i] && !b[i] && parseInt(a[i]) > 0) || (parseInt(a[i]) > parseInt(b[i]))) {
-            return 1;
-        } else if ((b[i] && !a[i] && parseInt(b[i]) > 0) || (parseInt(a[i]) < parseInt(b[i]))) {
-            return -1;
-        }
+    arr1 = convertToNumber(arr1) ;
+    arr2 = convertToNumber(arr2) ;
+
+    len = Math.max(arr1.length, arr2.length) ;
+    for (i=0; i<len; i ++) {
+
+        if( typeof arr1[i] === "undefined" )
+            return -1 ;
+        if( typeof arr2[i] === "undefined" )
+            return 1 ;
+
+        if( arr1[i] > arr2[i] )
+            return 1 ;
+        if( arr1[i] < arr2[i] )
+            return -1 ;
     }
-
-    return 0;
+    return 0 ;
 } ;
+
+
+
+/**
+ * Convert version array elements into numbers.
+ *
+ * @function
+ * @param {Array} arr - array of version string elements
+ * @return {Array} - converted array
+ */
+
+function convertToNumber(arr) {
+    return arr.map(function(el) {
+        return (typeof el === "number") ? parseInt(el) : el ;
+    }) ;
+}
